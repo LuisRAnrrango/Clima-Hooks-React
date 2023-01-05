8f401bf99a326a747ff644fd86294fc306afaefb
Luisito <luisitoylag@gmail.com>
2023-01-05 18:26:10 -0500

    Creacion de componente  Weather e integracion de Bootstrap y Route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import { Home } from "./pages/Home";
+import { Weather } from "./pages/Weather";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { Navbar } from "./components/Navbar";
+import { Route,Routes, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/tareas" element={<Weather />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
